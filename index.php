302f80ba74225be01a6d5a6eb1467e0f9a7f85e9
Muneeb Zia <luminite143@gmail.com>
2018-03-09 14:32:47 +0500

    first commit frontend

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MY PHP</title>
+	<meta http-equiv="X-UA-Compatible" content="IE-edge">
+  <meta name="viewport" content="width=device-width , initial-scale=1">
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.js"></script>
+</head>
+<body>
+<div class ="page-header" style="text-align:center";>
+<h1> Welcome To My Application </h1>
+<h5> <span class = "glyphicon glyphicon-cloud-upload" aria-hidden="true"> </h5>
+
+</body>
+</html>
